bee67c6601490b9aab2fe2aba173385fa1f95197
Sam Burnstone <sam.burnstone@gmail.com>
2015-06-07 20:05:18 +0100

    Adds new sections
    This commit includes section about variables (non-optionals and optionals)

--- SwiftQuickstart.playground/Contents.swift
+++ SwiftQuickstart.playground/Contents.swift
@@ -1,5 +1,82 @@
-//: Playground - noun: a place where people can play
+//: ## Quickfire introduction to Swift
+//: Inspired by http://learnxinyminutes.com/docs/swift/
 
+//: Swift imports all files within the current module automcatically
+
+//: When using other modules, we import them like:
 import UIKit
 
-var str = "Hello, playground"
+//: Printing to the console
+println("Hello everybody, I hope you aren't bored!")
+
+//: ## Variables
+//: Apple (and general programming guidelines) encourage the use of constants whenever we're certain a value shouldn't be mutated.
+//: Constants are represented using the *let* keyword:
+let constant = 40
+// COMPILER ERROR: constant = 10
+
+//: We don't have to immediately assign values to a constant, however once it has been assigned a value, we can't reassign one
+//: We also need to ensure it has a value before we can use it (we'll get a ghastly compilation error otherwise!)
+//: N.B. Swift tries its best to **infer** the type being assigned to a constant/variable. However, when we're not assigning anything immediately, we need to give it a bit of a helping hand
+let animal = "Beetle"
+let numberOfLegs: UInt
+
+if animal == "Beetle" {
+    numberOfLegs = 6
+}
+else {
+    numberOfLegs = 2
+}
+
+//: We can use *string interpolation* to directly print out values into a string. Objective-C develops rejoice!!!
+println("A \(animal) has \(numberOfLegs) legs")
+
+//: A small nicety of Swift is the ability to use any unicode character for a variable name
+let € = "Euro"
+let 🐷 = "Oink"
+
+println(🐷)
+
+//: ## Due to Swift being an inherently *safe* language, every variable (constant or otherwise) must have a value.
+//: Optionals provide an indication when a value could possibly be nil - if it ain't optional then it's almost certainly not nil
+var optionalString: String?
+
+println(optionalString)
+
+optionalString = "No longer nil"
+println(optionalString)
+
+if optionalString == "No longer nil" {
+    println("The optional contains a value")
+}
+
+//: If a variable is not marked as being optional (indicated by ?) then nil cannot be assigned to it
+var nonNilString = "Are you all still awake?"
+// COMPILER ERROR: nonNilString = nil
+
+ //: **Unwrapping** optionals to get at the sweet, sweet value contained within it.
+// So we can do this:
+if optionalString != nil {
+    // optional is not nil
+}
+
+// But how do we get the non-optional value out of it:
+if optionalString != nil {
+    println(optionalString)
+    println(optionalString!) // Force unwrap is safe as we know optionalString is not nil
+}
+
+//: Force unwrapping is a little dangerous if we just use it liberally without checking whether an optional contains a value
+var nilOptional: String?
+
+// RUNTIME ERROR: println(nilOptional!)
+
+//: There's a nicer way of unwrapping optional values...
+var stringToUnwrap: String? = "Beetle"
+println(stringToUnwrap)
+
+if let animal = stringToUnwrap {
+    println(animal) // No longer 'optional'
+}
+
+
