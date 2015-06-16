634d1d57d4e5b66a2528284e4d7a0392444dae68
Sam Burnstone <sam.burnstone@gmail.com>
2015-06-16 23:38:47 +0100

    Adds palindrome playground

---
+++ Palindrome.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Swift Homework - Palindrome
+
+func verifyPalindrome(stringToValidate: String) -> Bool
+{
+    let charactersArray = generateCharacterArrayFromCollection(stringToValidate.characters)
+    let reverseCharactersArray = generateCharacterArrayFromCollection(charactersArray.reverse())
+    
+    return charactersArray == reverseCharactersArray
+}
+
+func generateCharacterArrayFromCollection<T: CollectionType>(collection: T) -> [Character]
+{
+    var charactersArray = [Character]()
+    
+    for item in collection {
+        
+        if let item = item as? Character {
+            print("Item is a char")
+            charactersArray.append(item)
+        }
+    
+    }
+    
+    return charactersArray
+}
+
+//: Tests
+verifyPalindrome("hannah") // Expect true
+verifyPalindrome("sam") // Expect false
+verifyPalindrome("thisisiht") // Expect true
