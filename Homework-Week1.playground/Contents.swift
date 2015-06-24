34a6e650f5007d01d1b8c3107176c8055e806a15
Sam Burnstone <sam.burnstone@gmail.com>
2015-06-24 22:55:27 +0100

    Adds brackets and fizzbuzz implementations

--- Homework-Week1.playground/Contents.swift
+++ Homework-Week1.playground/Contents.swift
@@ -1,4 +1,6 @@
-//: Swift Homework - Palindrome
+//: Swift Homework
+
+//: Palindrome
 
 func verifyPalindrome(stringToValidate: String) -> Bool
 {
@@ -15,7 +17,6 @@ func generateCharacterArrayFromCollection<T: CollectionType>(collection: T) -> [
     for item in collection {
         
         if let item = item as? Character {
-            print("Item is a char")
             charactersArray.append(item)
         }
     
@@ -28,3 +29,90 @@ func generateCharacterArrayFromCollection<T: CollectionType>(collection: T) -> [
 verifyPalindrome("hannah") // Expect true
 verifyPalindrome("sam") // Expect false
 verifyPalindrome("thisisiht") // Expect true
+
+//: FizzBuzz
+let maximumValue = 100
+
+for index in 1...maximumValue {
+    
+    var outputString: String = ""
+     
+    // fizz if multiple of 3
+    if index % 3 == 0 {
+        outputString += "fizz"
+    }
+
+    // buzz if five
+    if index % 5 == 0 {
+        outputString += "buzz"
+    }
+    
+    print("\(index): \(outputString)")
+}
+
+//: Brackets
+
+func verifyValidBracketString(bracketedString: String) -> Bool
+{
+    var openBracketStack = [Character]()
+    
+    func openBracketCharacterFromClosedBracketCharacter(closedBracketCharacter: Character) -> Character?
+    {
+        switch closedBracketCharacter {
+        case "}": return "{"
+        case "]": return "["
+        case ")": return "("
+        default: break
+        }
+        
+        return nil
+    }
+    
+    for character in bracketedString.characters
+    {
+        switch character
+        {
+        case "{", "(", "[":
+            openBracketStack.insert(character, atIndex: 0)
+            openBracketStack
+        case "}", ")", "]":
+            // Compare to item at top of stack
+            if let characterAtTopOfStack = openBracketStack.first
+            {
+                if let openBracketCharacter = openBracketCharacterFromClosedBracketCharacter(character)
+                {
+                    if openBracketCharacter != characterAtTopOfStack {
+                        return false
+                    }
+                }
+            } else
+            {
+                return false
+            }
+            
+            openBracketStack.removeAtIndex(0)
+            print("Removing first item")
+            openBracketStack
+        default: break
+        }
+    }
+    
+    // Check whether we still have brackets that haven't been popped off the stack
+    if !openBracketStack.isEmpty
+    {
+        return false
+    }
+    
+    return true
+}
+
+// Tests
+verifyValidBracketString("") // Expect true
+verifyValidBracketString("()") // Expect true
+verifyValidBracketString("[]") // Expect true
+verifyValidBracketString("(([]{}))") // Expect true
+
+verifyValidBracketString("{") // Expect false
+verifyValidBracketString("{{{{{]") // Expect false
+
+
