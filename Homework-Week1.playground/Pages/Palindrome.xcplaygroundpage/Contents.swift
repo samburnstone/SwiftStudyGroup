d841775566eee8a6d652c49dfca877c673faaafe
Sam Burnstone <sam.burnstone@gmail.com>
2015-07-05 22:07:11 +0100

    Adds function toCharacterArray as protocol extension

--- Homework-Week1.playground/Pages/Palindrome.xcplaygroundpage/Contents.swift
+++ Homework-Week1.playground/Pages/Palindrome.xcplaygroundpage/Contents.swift
@@ -7,12 +7,13 @@
 */
 
 func verifyPalindrome(stringToValidate: String) -> Bool {
-    let charactersArray = generateCharacterArrayFromCollection(stringToValidate.characters)
-    let reverseCharactersArray = generateCharacterArrayFromCollection(charactersArray.reverse())
+    let charactersArray = stringToValidate.characters.toCharacterArray()
+    let reverseCharactersArray = charactersArray.reverse().toCharacterArray()
     
     return charactersArray == reverseCharactersArray
 }
 
+//: We can use a function to generate a character array from a string
 func generateCharacterArrayFromCollection<T: CollectionType>(collection: T) -> [Character] {
     var charactersArray = [Character]()
     
@@ -27,7 +28,24 @@ func generateCharacterArrayFromCollection<T: CollectionType>(collection: T) -> [
     return charactersArray
 }
 
-// Tests
-verifyPalindrome("hannah") // Expect true
-verifyPalindrome("sam") // Expect false
-verifyPalindrome("thisisiht") // Expect true
+//: Or we can use a protocol extension
+extension CollectionType {
+    func toCharacterArray() -> [Character] {
+        var charactersArray = [Character]()
+        
+        for item in self {
+            
+            if let item = item as? Character {
+                charactersArray.append(item)
+            }
+            
+        }
+        
+        return charactersArray
+    }
+}
+
+//: Tests
+verifyPalindrome("hannah")
+verifyPalindrome("thisisiht")
+verifyPalindrome("sam") == false
