167cc0a1c496185b44a5ec792d00e17c9aa51845
Sam Burnstone <sam.burnstone@gmail.com>
2015-07-18 10:09:02 +0100

    Adds basic closures to fizzbuzz exercise

--- Homework-Week2.playground/Pages/FizzBuzz.xcplaygroundpage/Contents.swift
+++ Homework-Week2.playground/Pages/FizzBuzz.xcplaygroundpage/Contents.swift
@@ -4,21 +4,40 @@
 */
 let maximumValue = 100
 
-for index in 1...maximumValue {
-    
-    var outputString: String = ""
-    
-    // fizz if multiple of 3
+//: We'll define an alias... because we can
+typealias outputResult = Int -> String
+
+//: Now for the core function
+func fizzBuzz(f: outputResult) {
+    for i in 1...maximumValue {
+        f(i)
+    }
+}
+
+//: Example without using trailing closure
+fizzBuzz({
+    index in
+    var output = ""
     if index % 3 == 0 {
-        outputString += "fizz"
+        output += "fizz"
     }
-    
-    // buzz if five
     if index % 5 == 0 {
-        outputString += "buzz"
+        output += "buzz"
     }
-    
-    print("\(index): \(outputString)")
+    return output
+})
+
+//: Example using trailing closure
+fizzBuzz() {
+    index in
+    var output = ""
+    if index % 3 == 0 {
+        output += "fizz"
+    }
+    if index % 5 == 0 {
+        output += "buzz"
+    }
+    return output
 }
 
 //: [Next](@next)
