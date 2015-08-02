44bc54486e66da96955e9fe7f9150d37f6d7169b
Sam Burnstone <sam.burnstone@gmail.com>
2015-08-02 22:04:54 +0100

    Fleshes out tennis example.
    It's a little bit of a mess at the moment, but it works.
    
    It doesn't know about sets yet... may do that later.

--- Homework-Week3.playground/Contents.swift
+++ Homework-Week3.playground/Contents.swift
@@ -58,55 +58,142 @@ func ==(lhs: Player, rhs: Player) -> Bool {
 
 let grumpyScot = Player(name: "Andy Murray", dateOfBirth: "1987-05-15")
 grumpyScot?.age
-let swissWithAHeadband = Player(name: "Roger Federer", dateOfBirth: "1981-08-08")
-swissWithAHeadband?.age
+let swissWithAHeadband = Player(name: "Roger Federer", dateOfBirth: "1981-08-08")!
+swissWithAHeadband.age
 
 let mysteryPlayer = Player(name: "Un Known", dateOfBirth: "") == nil
 
-let superSerb = Player(name: "Novak Djokovic", dateOfBirth: "1987-05-22")
+let superSerb = Player(name: "Novak Djokovic", dateOfBirth: "1987-05-22")!
 
 let bigRafa = Player(name: "Rafael Nadal", dateOfBirth: "1986-06-03", playingStyle: .LeftHanded)
 bigRafa?.age
 
 //: Tennis Match
 
-class Match {
-    let players: [Player]
-    var score = [Player: Int]()
+
+class Game {
+    
+    enum Point: Equatable {
+        case Numeric(value: Int)
+        case Advantage
+    }
+    
+    enum GameState {
+        case InProgress
+        case Won(winningPlayer: Player)
+        case Error
+    }
+    
+    typealias PlayerScores = [Player: Point]
+    
+    // Create an empty score dictionary
+    var playerScores = PlayerScores()
     
     init(players: [Player]) {
-        self.players = players
-        
-        // Set each player's score to 0
-        self.players.map({
-            score[$0] = 0
+        players.map({
+            playerScores[$0] = .Numeric(value: 0)
         })
     }
     
-    func awardPointTo(player: Player) {
+    func awardPointTo(player: Player) -> GameState {
         // Check a score exists for the given player
-        guard let currentScore = score[player] else { return }
-        
-        // Right, let's modify the current score
-        var updatedScore = currentScore
+        guard var currentPlayerScore = playerScores[player] else { return .Error }
         
         // Stupid tennis scoring system
-        switch currentScore {
-        case 0, 15: updatedScore += 15
-        case 30: updatedScore += 10
-        // TODO: case 40:
+        switch currentPlayerScore {
+            
+        case .Numeric(value: let value) where value <= 15:
+            currentPlayerScore = .Numeric(value: value + 15)
+            
+        case .Numeric(value: 30):
+            currentPlayerScore = .Numeric(value: 40)
+            
+        case .Numeric(value: 40):
+            
+            guard let opponent = retrieveOpponent(to: player) else { return .Error }
+            guard var opponentScore = playerScores[opponent] else { return .Error }
+            
+            switch opponentScore {
+            case .Numeric(value: 40): currentPlayerScore = .Advantage
+            case .Advantage: opponentScore = .Numeric(value: 40)
+            default: return .Won(winningPlayer: player)
+            }
+            
+            playerScores[opponent] = opponentScore
+            
+        case .Advantage: return .Won(winningPlayer: player)
+            
         default: break
         }
         
-        score[player] = updatedScore
+        playerScores[player] = currentPlayerScore
+        
+        return .InProgress
     }
     
+    // Search through playerScores' keys to retrieve other player
+    func retrieveOpponent(to player: Player) -> Player? {
+        return playerScores.keys.array.filter({ $0 != player }).first
+    }
 }
 
-let wimbledonFinal = Match(players: [superSerb!, swissWithAHeadband!])
-wimbledonFinal.score
+//: Swift doesn't automatically generate the enum equalities for us, so we'll cover those here
+func ==(lhs: Game.Point, rhs: Game.Point) -> Bool {
+    switch (lhs, rhs) {
+    case (.Numeric(let a),   .Numeric(let b))   where a == b: return true
+    case (.Advantage, .Advantage): return true
+    default: return false
+    }
+}
 
-wimbledonFinal.awardPointTo(superSerb!)
-wimbledonFinal.awardPointTo(swissWithAHeadband!)
-wimbledonFinal.score
+class Match {
+    
+    let players: [Player]
+    var games = [Game]()
+    
+    init(players: [Player]) {
+        self.players = players
+
+        games.append(Game(players: players))
+    }
+    
+    func awardPointTo(player: Player) {
+        guard let game = currentGame() else { return }
+        let gameState = game.awardPointTo(player)
+        
+        switch gameState {
+        case .Won(let winningPlayer):
+            print("\(winningPlayer.name) won the game!")
+            let newGame = Game(players: players)
+            games.append(newGame)
+        default: break
+        }
+    }
+    
+    /// Returns the current game in progress
+    func currentGame() -> Game? {
+        return games.last
+    }
+    
+    // TODO: Implement CustomStringConvertible and use reduce to determine number of games a player has won
+}
+
+let wimbledonFinal = Match(players: [superSerb, swissWithAHeadband])
+
+wimbledonFinal.awardPointTo(superSerb)
+wimbledonFinal.awardPointTo(superSerb)
+wimbledonFinal.awardPointTo(superSerb)
+
+wimbledonFinal.awardPointTo(swissWithAHeadband)
+wimbledonFinal.awardPointTo(swissWithAHeadband)
+wimbledonFinal.awardPointTo(swissWithAHeadband)
+
+wimbledonFinal.awardPointTo(superSerb) // What a beast!
+wimbledonFinal.currentGame()?.playerScores
+wimbledonFinal.awardPointTo(swissWithAHeadband)
+wimbledonFinal.currentGame()?.playerScores
+wimbledonFinal.awardPointTo(superSerb)
+wimbledonFinal.awardPointTo(superSerb)
+wimbledonFinal.currentGame()?.playerScores // Game has been reset
+
 
