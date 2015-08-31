08b1f0766e6c87571cba3c93a8e8f726e79cb83d
Sam Burnstone <sam.burnstone@gmail.com>
2015-08-31 15:06:55 +0100

    Adds fancier example of computed properties

--- Homework - Week 4.playground/Pages/ComputedProperty.xcplaygroundpage/Contents.swift
+++ Homework - Week 4.playground/Pages/ComputedProperty.xcplaygroundpage/Contents.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 //: Something simple to start us off
 //: A basic Range struct with a computed property
 struct SimpleRange {
@@ -25,5 +27,37 @@ largeRange.length
 
 // ERROR: let incorrectRange = SimpleRange(start: 10, end: 3)
 
+//: Let's be a little fancier. We'll create a protocol with a readonly computed property.
+protocol ThreeDShape {
+    // Read-only volume property
+    var volume: Double { get }
+}
+
+//: Create a struct that conforms to this protocol
+struct Cube: ThreeDShape {
+    var sideLength: Double = 10
+    
+    var volume: Double {
+        return pow(sideLength, 3)
+    }
+}
+
+var cube = Cube()
+cube.sideLength = 6
+cube.volume
+
+//: OOOOOHHHHHHHHH!!!!!!!
+let π = M_PI
+
+struct Sphere: ThreeDShape {
+    var radius = 0.0
+    
+    var volume: Double {
+        return 4/3 * π * pow(radius, 3)
+    }
+}
+
+var sphere = Sphere(radius: 5)
+sphere.volume
 
 //: [Next](@next)
