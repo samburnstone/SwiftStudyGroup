f6795509f92fc6c05e88cf2aec0ee396e5081d19
Sam Burnstone <sam.burnstone@gmail.com>
2015-10-10 22:22:03 +0100

    Adds extensions page to week 7 playground

---
+++ Homework Week 7.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+//: # Homework - Week 7
+
+//: Let's add a method to `String` that appends a happy face
+extension String
+{
+    // As we're altering 'self' we mark this method as mutating
+    mutating func appendMrHappy()
+    {
+        self += "😀" // Or could do ' self = "\(self)😀" '
+    }
+}
+
+var happyString = "I'm feeling happy happy happy"
+happyString.appendMrHappy()
+
+
+//: That was just spiffing! Let's try something a bit stranger...
+//: _________
+//: Add dynamic proprties to convert a double into other units
+extension Double
+{
+    var km: Double
+    {
+        return self / 1000
+    }
+    
+    var cm: Double
+    {
+        return self * 100
+    }
+
+    var miles: Double
+    {
+        return self.km * 0.621
+    }
+}
+
+// Now let's try it out
+let metresRan: Double = 5000
+metresRan.km
+metresRan.miles
+metresRan.cm
+
+//: How about passing in closures?
+
+extension Int
+{
+    func times(block: Void -> Void)
+    {
+        for _ in 0..<self
+        {
+           block()
+        }
+    }
+}
+
+3.times {
+    print("Is there an echo?")
+}
