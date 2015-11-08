1691577987ba6719ab933cbceac7bc66b9eb6368
Sam Burnstone <sam.burnstone@gmail.com>
2015-11-08 14:51:17 +0000

    Adds algorithm to find instances of banned words in message

--- Practical Playgrounds/NaughtyWordFilter.playground/Contents.swift
+++ Practical Playgrounds/NaughtyWordFilter.playground/Contents.swift
@@ -1,8 +1,9 @@
 //:# Naughty Word Filter
+import Foundation
 
 struct FlaggedWordToken
 {
-    let range: Range<Int>
+    let range: Range<String.CharacterView.Index>
     let word: String
 }
 
@@ -17,8 +18,28 @@ class BannedWordsParser
     
     func parse(message: String) -> [FlaggedWordToken]
     {
-        return []
+        let transformedString = message.lowercaseString // For now, assume no punctuation and allwe need to do is convert to lowercase
+        
+        let wordTokens = transformedString.characters.split { $0 == " " }.map(String.init)
+        
+        // Iterate through each word, keeping track of the current index
+        var index = transformedString.startIndex
+        var flaggedWordTokens = [FlaggedWordToken]()
+        
+        for word in wordTokens
+        {
+            if bannedWordsList.contains(word)
+            {
+                let rangeInMessage = index...(index.advancedBy(word.characters.count - 1))
+                flaggedWordTokens.append(FlaggedWordToken(range: rangeInMessage, word: word))
+            }
+            
+            index = index.advancedBy(word.characters.count + 1, limit: message.characters.endIndex)
+        }
+        
+        return flaggedWordTokens
     }
+    
 }
 
 //:## Banned Words Tests
