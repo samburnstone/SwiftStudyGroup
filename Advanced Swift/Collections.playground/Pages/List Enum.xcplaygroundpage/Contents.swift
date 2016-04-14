2a13ef9b3be434c4a37ac6c04ba2ef4c1c0b3238
Sam Burnstone <sburnstone@scottlogic.com>
2016-04-14 11:05:32 +0100

    Adds playground page for List collection
    Conforms to stack protocol

---
+++ Advanced Swift/Collections.playground/Pages/List Enum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Apple seem to be advocating a move away from naming protocols with a 'Type' suffix
+protocol StackType
+{
+    associatedtype Element
+    
+    mutating func pop() -> Element?
+    mutating func push(Element: Element)
+}
+
+enum List<Element>
+{
+    case End
+    indirect case Node(Element, next: List<Element>)
+}
+
+extension List
+{
+    /**
+     #### Quoting 'The Book':
+     *“We name this prepending method cons, because that is the name of the operation in LISP (it’s short for “construct,” and adding elements onto the front of the list is sometimes called “consing”).”*
+     
+     Excerpt From: Chris Eidhof. “Advanced Swift.” iBooks.
+     */
+    func cons(x: Element) -> List
+    {
+        return .Node(x, next: self)
+    }
+}
+
+extension List: StackType
+{
+    mutating func pop() -> Element? {
+        switch self
+        {
+        case .End: return nil
+        case let .Node(x, next: nextNode):
+            self = nextNode
+            return x
+        }
+    }
+    
+    mutating func push(element: Element) {
+        self = self.cons(element)
+    }
+}
+
+var l = List<Int>.End.cons(10).cons(20).cons(30)
+
+l.pop()
+l.pop()
+l.push(100)
