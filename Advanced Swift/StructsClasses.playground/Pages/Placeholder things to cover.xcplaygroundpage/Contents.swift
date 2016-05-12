7d56409df659eb197a9ea4e12a620049eac12b88
Sam Burnstone <sburnstone@scottlogic.com>
2016-05-12 12:31:23 +0100

    Adds placeholder for things to discuss

---
+++ Advanced Swift/StructsClasses.playground/Pages/Placeholder things to cover.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+### Swift 3 release schedule
+https://swift.org/blog/swift-3-0-release-process/
+ 
+- The first developer preview branch swift-3.0-preview-1-branch will be created from master on May 12. It will be released 4-6 weeks later.
+- First release to include the Swift Package Manager
+ 
+### `Weak` and `Unowned` references
+ 
+### `Closures` and reference cycles
+ 
+ */
+
+//: [Next](@next)
